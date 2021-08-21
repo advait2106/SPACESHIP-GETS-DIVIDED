0bbc8b3f42dc680a5ff1ac5ca06cd8c7a61bae93
advait2106 <76954638+advait2106@users.noreply.github.com>
2021-08-21 19:21:40 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# SPACESHIP-GETS-DIVIDED
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,23 @@
+var spaceship;
+var crewDragonCapsule;
+var boeingStarliner;
+
+function setup() {
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
+}
+
+function draw() {
+  background("red");  
+ 
+  spaceship= boeingStarliner+crewDragonCapsule
+  
+  if(spaceKeyIsPressed.spaceship=boeingStarliner%crewDragonCapsule)
+
+
+  drawSprites();
+createSprite.boeingStarliner(80,55,15,30);
+createSprite.crewDragonCapsule(120,45,10,45);
+createSprite.spaceship(200,100,25,75);
+
+}
